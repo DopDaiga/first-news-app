31c3152d85f1027a06eb5d639baa2f4b997ce6c6
DopDaiga <dopddaiga@gmail.com>
2019-06-21 15:57:44 +0200

    csv on html

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
+import csv
 from flask import Flask
 app = Flask(__name__)  # Note the double underscores on each side!
 
 from flask import render_template # import the render_template for use with html
 app = Flask(__name__)
 
+def get_csv():
+    csv_path = './static/la-riots-deaths.csv'
+    csv_file = open(csv_path, 'rb')
+    csv_obj = csv.DictReader(csv_file)
+    csv_list = list(csv_obj)
+    return csv_list
+
 # returns the rendered index.html template
 @app.route("/") # connects that function with the root URL of our site,/
 def index():
     template = 'index.html'
-    return render_template(template)
+    object_list = get_csv()
+    return render_template(template, object_list=object_list)
 
 if __name__ == '__main__':
     # Fire up the Flask test server
